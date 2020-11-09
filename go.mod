109e813d95e11923904f89baefa0d6b68a58ec4e
amitnakod <amit53nakod@gmail.com>
2020-11-09 16:45:23 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.14
+
+require github.com/mailjet/mailjet-apiv3-go v0.0.0-20201009050126-c24bc15a9394
---
+++ mailModels/mailModels.go
@@ -0,0 +1,9 @@
+package mailModels
+
+
+type mailtemplate struct {
+  to string
+  cc string 
+  Bcc string 
+  message string 
+}
